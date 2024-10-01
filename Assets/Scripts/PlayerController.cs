8f9ba3104da1d72118d80e67fdc309bf30a3e3fa
yanagikaito <voltgt777@gmail.com>
2024-10-01 21:09:09 +0900

    再生を押すとセルの中央にキャラクターを移動するようにしました。

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // 変数作成(移動スピード,アニメーション,胴体)
-    // キー入力を受け取り移動させるコード記述
-    // 方向とアニメーションを連動
+    // 移動に使う場合はグリッドへの参照が必要
+    // グリッドプロパティ
+    public Grid Grid { get; set; }
+
+    // 移動しているときに設定する
+    // グリッドを使用したい場合は必ずグリッドへの参照を取得する必要がある
+    private void Awake()
+    {
+        Grid = FindObjectOfType<Grid>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Vector3Int currentCell = Grid.WorldToCell(transform.position);
+        transform.position = Grid.GetCellCenterWorld(currentCell);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
 
+        }
+        //// セルの中心をベクトル3として返す
+        //Grid.GetCellCenterWorld();
+
+        //// ベクトル3の位置を取得し,グリッド上のセルをベクトルとして返す
+        //Grid.WorldToCell();
     }
 }
